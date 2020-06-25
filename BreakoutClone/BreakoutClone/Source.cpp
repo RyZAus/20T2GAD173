d75bd99390c14d8e46530af657ecc21339f03a78
Riley McGowan <1017093@student.sae.edu.au>
2020-06-25 10:50:57 +1000

    Initial Breakout Setup
    Completed the screen bounds, paddle, and ball.

--- BreakoutClone/BreakoutClone/Source.cpp
+++ BreakoutClone/BreakoutClone/Source.cpp
@@ -1,39 +1,112 @@
 #include <iostream>
-//declare initial int "x" to 0
-int x = 0;
+#include <conio.h>
+#include <SFML/Graphics.hpp>
+///Declare all of our int's
+int windowX = 1000; //initial window width
+int windowY = 600; //initial window height
+int speed = 10; //how fast we move
 int main()
 {
-	//prints hello world to make sure the code runs
-	std::cout << "Hello World" << std::endl;
-	// adds a condition so if "x" is under 100 continue to loop
-	while (x < 100)
+	///shape stuff
+	sf::Vector2f rectSize = sf::Vector2f(150.f, 50.f); //size of our paddle in pixel size
+	sf::Vector2f rectPos = sf::Vector2f((windowX / 2) - (rectSize.x / 2), (windowY / 1.5f) - (rectSize.y / 2)); //center our paddle
+	sf::Color rectColour = sf::Color(255, 255, 0, 255); //sets our colour r/g/b/a
+	//Defining the window, name, and frame rate
+	sf::RenderWindow window(sf::VideoMode(windowX, windowY), "Breakout Clone"); //give our window a name and perameters by default
+	window.setFramerateLimit(60); //stop our window from running 500 fps
+	//Setting all of our paddle settings
+	sf::RectangleShape paddle(rectSize); //Our paddle object
+	paddle.setFillColor(rectColour); //set colour of paddle
+	paddle.setPosition(rectPos); //set position of paddle
+	///ball stuff
+	sf::CircleShape ball(10.f); //create ball shape
+	ball.setFillColor(sf::Color::Cyan); //colour set
+	sf::Vector2f velocity = sf::Vector2f(0.f, 0.f); //setup velocity
+	velocity.x = rand() % 10; //generate a number between 1 and 10 to make x random initially
+	velocity.y = -5; //move up by default
+	//move starting pos of the ball up by the height of the paddle
+	ball.setPosition(sf::Vector2f(rectPos.x, rectPos.y - rectSize.y));	
+	//code to run when the window is open
+	///border stuff
+	sf::RectangleShape top;
+	sf::RectangleShape bottom;
+	sf::RectangleShape left;
+	sf::RectangleShape right;
+	//sets position to vector2f aka co-ordinates and x, y
+	top.setSize(sf::Vector2f(windowX, 1));
+	top.setPosition(sf::Vector2f(0, 0));
+	top.setFillColor(sf::Color::Black);
+	bottom.setSize(sf::Vector2f(windowX, 1));
+	bottom.setPosition(sf::Vector2f(0, windowY - 1));
+	bottom.setFillColor(sf::Color::Black);
+	left.setSize(sf::Vector2f(windowX, 1));
+	left.setPosition(sf::Vector2f(0, 0));
+	left.setFillColor(sf::Color::Black);
+	right.setSize(sf::Vector2f(windowX, 1));
+	right.setPosition(sf::Vector2f(windowX - 1, 0));
+	right.setFillColor(sf::Color::Black);
+	//run this when the window is open
+	while (window.isOpen()) //IF YOU WANT TO SHRINK CODE USE THIS PLUS
 	{
-		//increase "x" by 1
-		x++;
-		//if condition for if "x" is equal to either 3 or 5 continue down the line of if statement
-		if (x % 3 == 0 || x % 5 == 0)
+		//check if left is pressed
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
 		{
-			//if "x" is equal to a multiple of 3 then type fizz then proceed
-			if (x % 3 == 0)
+			//move rectangle left by decreasing
+			rectPos.x -= speed;
+			if (rectPos.x <= 0) 
 			{
-				std::cout << "Fizz";
+				rectPos.x = 0;
 			}
-			//if "x" is equal to a multiple of 5 then type buzz and end line
-			if (x % 5 == 0)
-			{
-				std::cout << "Buzz" << std::endl;
-			}
-			//otherwise end line
-			else
-			{
-				std::cout << std::endl;
-			}
+			//set the rectangle position for return
+			paddle.setPosition(rectPos);
 		}
-		//if the answer is not a multiple of 3 or 5 print "x"
-		else
+		//check if right is pressed
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
 		{
-			std::cout << x << std::endl;
+			//move rectangle right by increasing
+			rectPos.x += speed;
+			if (rectPos.x >= windowX - rectSize.x)
+			{
+				rectPos.x = windowX - rectSize.x;
+			}
+			//set the rectangle position for return
+			paddle.setPosition(rectPos);
 		}
+		if (top.getGlobalBounds().intersects(ball.getGlobalBounds())) 
+		{
+			//did we hit the top?
+			velocity.y = -velocity.y;
+
+		}
+		if (bottom.getGlobalBounds().intersects(ball.getGlobalBounds()))
+		{
+			//did we hit the bottom?
+			velocity.y = -velocity.y;
+		}
+		if (left.getGlobalBounds().intersects(ball.getGlobalBounds()) || right.getGlobalBounds().intersects(ball.getGlobalBounds()))
+		{
+			//did we hit the side?
+			velocity.x = -velocity.x;
+		}
+		if (paddle.getGlobalBounds().intersects(ball.getGlobalBounds()))
+		{
+			//did we hit the bottom?
+			velocity.y = -velocity.y;
+		}
+		ball.move(velocity); //move ball around
+		//pollevent is our window updating event
+		sf::Event event;
+		while (window.pollEvent(event)) 
+		{
+			//if window closes identify
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		//clear our window, and redraw everything
+		window.clear();
+		window.draw(paddle);
+		window.draw(ball);
+		window.display();
 	}
 	return 0;
 }
